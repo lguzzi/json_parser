8d2cf83f086d1294c28381c5d14864172e110def
Luca Guzzi <luca.guzzi@mib.infn.it>
2019-05-16 15:31:46 +0200

    first commi
        json formatting and output implemented
        int, float, bool and char* values implemented
    
    	new file:   include/json.h
    	new file:   src/json.cpp

---
+++ include/json.h
@@ -0,0 +1,47 @@
+#ifndef NOD_CLS
+#define NOD_CLS
+
+#include <string>
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include <list>
+#include <typeinfo>
+
+namespace json_options{
+    extern int  INDENTATION ;
+}
+
+class json{
+    public:
+        json();
+        json(const char* main_label);
+       ~json(); 
+        
+        std::list<std::string>& keys();
+
+        void _add_child (json* child);
+
+        json&               operator[](const char*  node_name) ;
+        void                    operator= (const char*  content  ) ;
+        void                    operator= (const float  content  ) ;
+        void                    operator= (const double content  ) ;
+        void                    operator= (const bool  content   ) ;
+        void                    operator= (const int    content  ) ;
+
+        friend std::ostream&    operator<<(std::ostream& stream, json& node) ;
+    
+    private:
+        std::vector<json*>  _subnodes   ;
+        std::list<std::string>  _keys       ;
+        std::string             _content    ;
+        std::string             _label      ;
+        std::string             _type       ;
+
+        bool    _last   ;
+        int     _depth  ;
+};
+
+std::ostream& operator<<(std::ostream& os, json& node) ;
+
+#endif
---
+++ src/json.cpp
@@ -0,0 +1,108 @@
+#include "../include/json.h"
+
+#include <string>
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+#include <list>
+#include <csignal>
+#include <typeinfo>
+
+namespace json_options{
+    int  INDENTATION = 4     ;
+}
+
+json::json(){
+    _last   = true ;
+    _depth  = 0    ;
+}
+json::json(const char* main_label){
+    _label = main_label ;
+    _depth = 0 ;
+    _last  = true ; 
+}
+json::~json(){
+}
+
+json& json::operator[] (const char* label){
+    for (std::vector<json*>::iterator it = _subnodes.begin(); it != _subnodes.end(); ++it){
+        if ((*it)->_label == std::string(label)){
+            return *(*it);
+        }
+    }
+    _add_child(new json(label)) ;
+    return *(_subnodes.back()) ;
+}
+
+void json::operator=(const char*  content){ 
+    _content = std::string   (content) ; 
+    _type = "str" ;
+}
+void json::operator=(const float  content){ 
+    _content = std::to_string(content) ; 
+    _type = "flt" ;
+}
+void json::operator=(const double  content){ 
+    _content = std::to_string(content) ; 
+    _type = "flt" ;
+}
+void json::operator=(const int    content){ 
+    _content = std::to_string(content) ; 
+    _type = "int" ;
+}
+void json::operator=(const bool   content){ 
+    _content = std::to_string(content) ; 
+    _type = "boo" ;
+}
+
+void json::_add_child(json* child){
+    _type = "jsn" ;
+    if (_subnodes.size()) _subnodes.back()->_last = false ;
+    _subnodes.push_back(child)  ;
+    _keys.push_back(std::string(child->_label)) ;
+    child->_depth = _depth + 1 ;
+}
+
+std::list<std::string>& json::keys()   { return _keys ; }
+
+std::ostream& operator<<(std::ostream& stream, json& node){
+    if (node._depth != 0){
+        if      (node._type == "jsn"){  stream  << std::string(node._depth * json_options::INDENTATION, ' ')
+                                                << "\"" << node._label      << "\": " 
+                                                << "{"  << std::endl;
+        }
+        else if (node._type == "str"){  stream  << std::string(node._depth * json_options::INDENTATION, ' ')
+                                                << "\"" << node._label      << "\": " 
+                                                << "\"" << node._content    << "\"" ;
+        }
+        else if (node._type == "flt"){  stream  << std::string(node._depth * json_options::INDENTATION, ' ')
+                                                << "\"" << node._label      << "\": " 
+                                                << std::stof(node._content);
+        }
+        else if (node._type == "int"){  stream  << std::string(node._depth * json_options::INDENTATION, ' ')
+                                                << "\"" << node._label      << "\": " 
+                                                << std::stoi(node._content);     
+        }
+        else if (node._type == "boo"){  stream  << std::string(node._depth * json_options::INDENTATION, ' ')
+                                                << "\"" << node._label      << "\": ";
+                                                if (std::stoi(node._content))   stream << "true" ;
+                                                else                            stream << "false";
+        }
+        else if (node._type == "vec"){  stream  << std::string(node._depth * json_options::INDENTATION, ' ')
+                                                << "\"" << node._label      << "\": "
+                                                << "TBA";
+        }
+    } else{
+        stream << "{" << std::endl;
+    }
+    
+    for (std::vector<json*>::iterator it = node._subnodes.begin(); it != node._subnodes.end(); ++it){
+        stream << *(*it) ;
+    }        
+    if (node._type == "jsn")    stream  << std::string(node._depth * json_options::INDENTATION, ' ')
+                                        << "}" ;
+    if (! node._last)           stream  << "," ;
+
+    return stream << std::endl;
+}
